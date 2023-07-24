628a4bf8bceb203f1ee15494eef5895257d50472
anjxxyi <119813212+anjxxyi@users.noreply.github.com>
2023-07-24 18:14:21 +0900

    1. Setting&Test : mybatis, JPA 연동

---
+++ java-spring/test/java/com/anjxxyi/practice/boilerplate/BolierplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjxxyi.practice.boilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolierplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
