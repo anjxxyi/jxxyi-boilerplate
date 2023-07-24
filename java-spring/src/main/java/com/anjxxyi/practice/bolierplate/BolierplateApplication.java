0a36ed79ec4c74c7de94e5b8a63861f52e79b718
anjxxyi <anjxxyi@gmil.com>
2023-07-24 10:58:14 +0900

    1. Setting 'SpringBoot'

---
+++ java-spring/src/main/java/com/anjxxyi/practice/bolierplate/BolierplateApplication.java
@@ -0,0 +1,13 @@
+package com.anjxxyi.practice.bolierplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolierplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolierplateApplication.class, args);
+	}
+
+}
